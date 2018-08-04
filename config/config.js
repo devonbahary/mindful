7bb2e1b8ce27e6a41e53390a24ebeb3a8686dbd5
devonbahary <devonbahary@gmail.com>
2018-08-03 19:51:00 -0700

    Add MONGODB_URI for mLab prod

--- config/config.js
+++ config/config.js
@@ -2,6 +2,8 @@ if (process.env.NODE_ENV === 'test') {
   process.env.MONGODB_URI = 'mongodb://localhost:27017/noteable-test';
 } else if (process.env.NODE_ENV === 'dev') {
   process.env.MONGODB_URI = 'mongodb://localhost:27017/noteable-dev';
+} else {
+  process.env.MONGODB_URI = 'mongodb://heroku_6c9vnq44:ubcbuod8u5t75clnn0j1ttkcl0@ds141621.mlab.com:41621/heroku_6c9vnq44';
 }
 
 if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'dev') {
